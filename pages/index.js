23bd1364dc412db6fa6eb8c1f21ff7b572a1d252
aiandgreat <ajsgarcia.student@ua.edu.ph>
2025-08-14 15:02:13 +0800

    second commit

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello I am Aian Jae Garcia. I am an aspiring cybersecurity analyst and web developer</p>
+        <p>Hello I am Aian Jae Garcia. I am an aspiring cybersecurity analyst and web developer. I am from the University of the Assumption</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
